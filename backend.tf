87b1cf640bb1cd9c300af7042c8ce76a546fe5b5
Akash-HC <akashchalkodu@gmail.com>
2024-08-14 17:02:47 +0530

    files moved to project folder

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-project-final" #replace with your bucket name
-    key = "Project/terraform.tfstate" #mention the destination folder to store the state file
-    region = "us-east-1" 
-  }
-}
